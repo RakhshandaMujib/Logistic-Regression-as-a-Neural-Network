b79235d2e88205bd443a2cbea0692ac4853f35dc
Rakhshanda Mujib <47360491+RakhshandaMujib@users.noreply.github.com>
2023-08-22 01:55:34 +0530

    Update driver.py

--- driver.py
+++ driver.py
@@ -4,9 +4,17 @@ from logistic_regression import *
 def main():
   #Load the Iris Dataset:
   X_train, X_test, y_train, y_test = load_iris()
+
+  #Create the model object:
   model = LogisticRegression(X_train, y_train, iters = 1000, alpha = 0.005)
+  
+  #Train the classifier:
   model.fit(print_cost = True)
+  
+  #Predict the test cases:
   y_pred = model.predict(X_test)
+  
+  #Evaluate the classifier:
   model.model_eval(y_pred, y_test)
   model.plot_cost()
 
